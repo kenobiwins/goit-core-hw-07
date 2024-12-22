e4bc7753153539da5241b105bf1b9193a807c3f9
Maksym Sydorenko <maksym.sy96@gmail.com>
2024-12-22 19:28:49 +0200

    refactor: Remove unused decorators and streamline imports in handler_decorators

--- decorators/__init__.py
+++ decorators/__init__.py
@@ -1,10 +1,6 @@
 from .handler_decorators import (
-    check_contact_existence,
-    check_contact_exists,
+
     command_error_handler,
-    ensure_contacts_available,
-    ensure_two_args,
-    ensure_valid_phone_number,
     handle_input_error,
 )
 
