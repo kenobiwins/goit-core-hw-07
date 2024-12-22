07c72a6d107ab58bcc81e9f88b00f0e554dfd424
Maksym Sydorenko <maksym.sy96@gmail.com>
2024-12-22 11:57:32 +0200

    feat: Add BirthdayValidation class to validate birthday format and values

---
+++ validation/BirthdayValidation.py
@@ -0,0 +1,23 @@
+import re
+from datetime import datetime
+
+from utils import BirthdayHelper
+from .ValidationStrategy import ValidationStrategy
+
+
+class BirthdayValidation(ValidationStrategy):
+    def validate(self, value):
+        regex = r"^[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{4}$"
+        if not value.strip():
+            raise ValueError("Birthday couldn't be empty")
+
+        if not re.match(regex, value):
+            raise ValueError("Invalid birthday format")
+
+        try:
+            parsed_date = datetime.strptime(value, BirthdayHelper.DATE_FORMAT)
+
+            return value if parsed_date.strftime(BirthdayHelper.DATE_FORMAT) == value else False
+
+        except ValueError:
+            return False
--- validation/__init__.py
+++ validation/__init__.py
@@ -1,4 +1,5 @@
 from .NameValidation import NameValidation
 from .PhoneValidation import PhoneValidation
+from .BirthdayValidation import BirthdayValidation
 
-__all__ = ["NameValidation", "PhoneValidation"]
+__all__ = ["NameValidation", "PhoneValidation", "BirthdayValidation"]
