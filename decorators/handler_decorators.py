25f450d53f9b45381fa2f5a6b8f008972fb92990
Maksym Sydorenko <maksym.sy96@gmail.com>
2024-12-22 16:13:49 +0200

    feat: Add command error handler decorator for improved command validation

--- decorators/handler_decorators.py
+++ decorators/handler_decorators.py
@@ -1,11 +1,12 @@
 from functools import wraps
-from typing import Callable, Any
+from typing import Callable, Any, List, Dict
 from enums import Color
 
 def handle_input_error(func: Callable[..., str]) -> Callable[..., str]:
     @wraps(func)
     def handler(*args: Any, **kwargs: Any) -> str:
         try:
+
             return func(*args, **kwargs)
         except KeyError:
             return f"{Color.ERROR.value}Error: Contact not found."
@@ -13,5 +14,18 @@ def handle_input_error(func: Callable[..., str]) -> Callable[..., str]:
             return f"{Color.ERROR.value}Error: {e}"
         except IndexError:
             return f"{Color.ERROR.value}Error: Not enough arguments provided. Two arguments (name and phone) are required."
+
+    return handler
+
+def command_error_handler(func: Callable[[str, Any], Any]) -> Callable[[str, Any], Any]:
+    @wraps(func)
+    def handler(command: str, *args: Any, **kwargs: Any) -> Any:
+        try:
+            return func(command, *args, **kwargs)
+        except ValueError:
+            print(
+                f"{Color.ERROR.value}Error: Invalid command. Type 'help' to see available commands for the CLI."
+            )
+            return None
 
     return handler
